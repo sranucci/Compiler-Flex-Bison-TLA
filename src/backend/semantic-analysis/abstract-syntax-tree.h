263150f20e7294d2f1176f65d781237f32691065
sranucci <sranucci@itba.edu.ar>
2023-05-30 14:51:55 -0300

    Started Adding AST structs

--- src/backend/semantic-analysis/abstract-syntax-tree.h
+++ src/backend/semantic-analysis/abstract-syntax-tree.h
@@ -7,6 +7,25 @@
 * de otras 2 expresiones.
 */
 typedef struct Expression Expression;
+typedef struct ProgramNode ProgramNode;
+typedef struct ExpressionsNode ExpressionsNode;
+typedef struct ExpressionNode ExpressionNode;
+typedef struct ChartTypeNode ChartTypeNode;
+typedef struct ChartType1Node ChartType1Node;
+typedef struct ChartType2Node ChartType2Node;
+typedef struct AddDatas AddDatas;
+typedef struct Data Data;
+typedef struct YData YData;
+typedef struct Color Color;
+typedef struct SetAxis SetAxis;
+typedef struct Interval Interval;
+typedef struct IntervalWithStep IntervalWithStep;
+typedef struct ValueList ValueList;
+typedef struct StepLeft StepLeft;
+typedef struct StepRight StepRight;
+typedef struct Value Value;
+typedef struct Function Function;
+typedef struct Number Number;
 
 /**
 * Para cada no-terminal se define una nueva estructura que representa su tipo
@@ -25,6 +44,110 @@ typedef struct {
 * De este modo, al recorrer el AST, es posible determinar qué nodos hijos
 * posee según el valor de este enumerado.
 */
+
+struct ProgramNode {
+	ExpressionsNode * expressionsNode;
+};
+
+struct ExpressionsNode {
+	ExpressionNode * expressionNode;
+	ExpressionsNode * expressionsNode;
+};
+
+
+struct ExpressionNode {
+	ChartTypeNode * chartTypeNode;
+}; 
+
+
+typedef enum {
+	CHARTTYPE1,
+	CHARTTYPE2
+} ChartType;
+
+
+struct ChartTypeNode {
+	//---------first option
+	ChartType * chartType;
+	ChartType1Node * chartType1Node;
+	Data * data;
+	YData * yData;
+	Color * color;
+	SetAxis * setAxis;
+
+
+	//---------Second option
+	ChartType2Node * chartType2Node;
+	AddDatas * addDatas;
+
+};
+
+
+struct ChartType1Node {
+	//put constants
+};
+
+
+struct ChartType2Node {
+	//put constatns
+};
+
+
+struct StepLeft{
+
+};
+
+struct StepRight {
+
+};
+
+
+struct ValueList {
+	Value * value;
+};
+
+struct Value {
+
+};
+
+struct YData {
+	Function * function;
+
+};
+
+struct AddData {
+	Number * number;
+	Color * color;
+};
+
+
+
+
+
+typedef enum {
+	REGULAR,
+	WITHSTEP,
+	VALUELIST
+} IntervalType;
+
+
+struct Data {
+	IntervalType intervalType;
+	Interval * interval;
+	IntervalWithStep * intervalWithStep;
+	ValueList * valueList;
+};
+
+
+
+
+
+
+
+
+
+
+//=-----------------Cosas catedra
 typedef enum {
 	EXPRESSION,
 	CONSTANT
@@ -34,6 +157,16 @@ typedef struct {
 	FactorType type;
 	Expression * expression;
 } Factor;
+
+
+
+
+
+
+
+
+
+
 
 typedef enum {
 	ADDITION,
